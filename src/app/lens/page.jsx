0c9a9a5b1a4a0e60b6abcdb59eebee42d066b4fd
Yash Sharma <syash5575@gmail.com>
2024-11-20 22:28:08 +0530

    fix: wrap page in suspense boundary

--- src/app/lens/page.jsx
+++ src/app/lens/page.jsx
@@ -2,12 +2,15 @@
 import { useSearchParams } from 'next/navigation';
 import Navbar from './components/Navbar';
 import MainSection from './components/MainSection';
+import { Suspense } from 'react';
 
 export default function Lens() {
   return (
     <div>
-      <Navbar />
-      <MainSection />
+      <Suspense>
+        <Navbar />
+        <MainSection />
+      </Suspense>
     </div>
   );
 }
